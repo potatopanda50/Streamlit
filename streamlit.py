a9be4e559181c032e0115b11639e3874af8b897b
potatopanda50 <potatopanda50@gmail.com>
2022-10-15 22:23:29 -0500

    1

--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,14 @@
 import streamlit as st
+import pandas as pd
+
+from gsheetsdb import connect
+
+sheet_id = "1nMeqYPSty9V8cLPzg95X1tJ7MWLy_cn15cTRcoS7R1k"
+sheet_name = "Sheet1"
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+
+data = pd.read_csv(url)
 
 st.title("hellow")
+
+st.dataframe(data=data, width=None, height=None, use_container_width=False)
